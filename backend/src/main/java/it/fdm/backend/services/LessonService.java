ab1c850e28653cfeddf99059e60eee3f145d9d73
markcasf <marcasfdm@gmail.com>
2023-12-01 17:10:00 +0100

    add service

--- backend/src/main/java/it/fdm/backend/services/LessonService.java
+++ backend/src/main/java/it/fdm/backend/services/LessonService.java
@@ -1,5 +1,13 @@
 package it.fdm.backend.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LessonService {
+	
+	public void metodo (String videoId, String userId) {
+		
+		
+	}
 
 }
